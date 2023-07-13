397361be91a44102369978e71a7e7c5034f4c001
Alexander <piachev.a@ya.ru>
2023-07-13 14:41:46 +0900

    MEDIA set proxy

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -115,6 +115,9 @@ STATIC_ROOT = BASE_DIR / 'collected_static' / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
